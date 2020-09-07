b5d61f08e3244ca062d849b4dd809343c68f4f06
Katheryn J Satterlee <ksatter@pop-os.localdomain>
2020-09-07 12:40:59 -0500

    Updates

---
+++ precache-manifest.8e8c575e609e6e436a10f222568553c7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a242f62986c5f5c9e87d3ae25b4fbaae",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "074e6efcbc07ebb9136a",
+    "url": "/React-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19e7dafe2ea3da0fe365",
+    "url": "/React-Portfolio/static/js/2.3d2a4c1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.3d2a4c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074e6efcbc07ebb9136a",
+    "url": "/React-Portfolio/static/js/main.6b6bd85d.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "f897236b8d6c2ca4e05719fc7902a674",
+    "url": "/React-Portfolio/static/media/Herosheet.f897236b.png"
+  },
+  {
+    "revision": "71ca6797a846103fa39e66af22826ab0",
+    "url": "/React-Portfolio/static/media/NoteTakerhome.71ca6797.png"
+  },
+  {
+    "revision": "48ff25e75b741ad93649f3a67c7b69c5",
+    "url": "/React-Portfolio/static/media/heroabilities.48ff25e7.png"
+  },
+  {
+    "revision": "42474f10e5a5ae32e9af4539b0f5f684",
+    "url": "/React-Portfolio/static/media/herorace.42474f10.png"
+  },
+  {
+    "revision": "923e73e051f0eb3e9688d864df6a6c36",
+    "url": "/React-Portfolio/static/media/heroroster.923e73e0.png"
+  },
+  {
+    "revision": "9a779ae773e52ad7bef8a956afc270dc",
+    "url": "/React-Portfolio/static/media/herosplash.9a779ae7.png"
+  },
+  {
+    "revision": "6cd49b4fd8c7ea4afce6beb27ae7883e",
+    "url": "/React-Portfolio/static/media/linkedInIcon.6cd49b4f.ico"
+  },
+  {
+    "revision": "707c1f1d9c5e1cf3510800464d5d4797",
+    "url": "/React-Portfolio/static/media/notesheet.707c1f1d.png"
+  },
+  {
+    "revision": "07166748a0e1b7bb13394896aea3063b",
+    "url": "/React-Portfolio/static/media/notesplash.07166748.png"
+  },
+  {
+    "revision": "8f586d449ab1614cb2cea858f2a9db2c",
+    "url": "/React-Portfolio/static/media/roadsplash.8f586d44.png"
+  },
+  {
+    "revision": "24808f0c08bc980559cfa1c062fc3bd5",
+    "url": "/React-Portfolio/static/media/roadtrippr.24808f0c.png"
+  },
+  {
+    "revision": "d68f92452ba3e67f5e719aff614725ba",
+    "url": "/React-Portfolio/static/media/roadtripprpage.d68f9245.png"
+  },
+  {
+    "revision": "6dbb2722302448e679d17d1b00496fe0",
+    "url": "/React-Portfolio/static/media/slashaadmin.6dbb2722.png"
+  },
+  {
+    "revision": "b61addd0b6ce8fe8c3bf32c328b48f67",
+    "url": "/React-Portfolio/static/media/slashahome.b61addd0.png"
+  },
+  {
+    "revision": "2266e9f93895d54768cfa4516c321743",
+    "url": "/React-Portfolio/static/media/slashamenu.2266e9f9.png"
+  }
+]);
